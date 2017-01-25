f74656808f140e2f4bce22bcab21e4f56f8489a9
wilder85 <juanalverto@gmail.com>
2017-01-25 14:10:26 -0600

    First RA commit
    First RA commit

---
+++ Assets/MaterialCurso/Scripts/ControlEventos.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlEventos : MonoBehaviour 
+{
+
+  public Collider web;
+  public Collider mail;
+  public Collider face;
+  public Collider twit;
+
+	void Start () 
+  {
+	
+	}
+	
+	void Update () 
+  {
+    if (Input.GetButtonDown("Fire1"))
+    {
+       Ray ray  = Camera.main.ScreenPointToRay(Input.mousePosition);
+       RaycastHit hit = new RaycastHit();
+
+
+        if(web.Raycast(ray,out hit,10000f))
+         {
+           Application.OpenURL("http://wantmake.it");
+         }
+         else if(mail.Raycast(ray,out hit,10000f))
+         {
+           string email = "juanalverto@gmail.com";
+           string subject = MyEscapeURL("Hola Jay");
+           string body = MyEscapeURL("Saludos\r\nEstoy viendo tu trabajo");
+           Application.OpenURL("mailto:" + email + "?subject=" + subject + "&body=" + body);
+         }
+         else if(face.Raycast(ray,out hit,10000f))
+         {
+           Application.OpenURL("https://www.facebook.com/juanalverto");
+         }
+         else if(twit.Raycast(ray,out hit,10000f))
+         {
+           Application.OpenURL("https://twitter.com/MyRadicalTV");
+         }
+      }
+    }
+
+
+  string MyEscapeURL (string url)
+  {
+    return WWW.EscapeURL(url).Replace("+","%20");
+  }
+}
